646323e8a7dbe8e4c251f800cc4a3c509902b3a7
Nobsmoke123 <donaldakobundu@gmail.com>
2025-05-29 19:16:23 +0100

    feat: add StartupCard component and implement startup listing in homepage, enhance date formatting utility

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query || "";
+
+  const posts = [
+    {
+      _id: 1,
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Chidera Ejimofor" },
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1742827871494-3a34fc06b69f?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -20,6 +35,20 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-26-semibold">
+          {query ? `Search results for ${query}` : `All Startups`}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartupCard key={post._id} {...post} />)
+          ) : (
+            <li className="card">No startups found</li>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,80 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+export type StartupCardProps = {
+  _id: number;
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+  author: { _id: number; name: string };
+  views: number;
+  _createdAt: Date;
+};
+
+const StartupCard: React.FC<StartupCardProps> = ({
+  _id,
+  _createdAt,
+  views,
+  author,
+  description,
+  image,
+  category,
+  title,
+}) => {
+  return (
+    <li className="startup-card group" key={_id}>
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author._id}`}>
+            <p className="text-16-medium line-clamp-1">{author.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author._id}`}>
+          <Image
+            src={"https://placehold.co/48x48"}
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={image} alt="startup-banner" className="startup-card_img" />
+
+        <div className="flex-between gap-3 mt-5">
+          <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className="text-16-medium">{category}</p>
+          </Link>
+
+          <Button className="startup-card_btn" asChild>
+            <Link href={`/startup/${_id}`} className="text-white">
+              Details
+            </Link>
+          </Button>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default StartupCard;
