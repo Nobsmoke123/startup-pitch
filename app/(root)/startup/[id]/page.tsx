e109cd01cfd03c2126f574bd3c612b0135e6606c
Nobsmoke123 <donaldakobundu@gmail.com>
2025-06-14 21:17:36 +0100

    feat: enhance startup page to fetch and display editor picks concurrently

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,10 @@
-import { StartUpTypeCard } from "@/components/StartupCard";
+import StartupCard, { StartUpTypeCard } from "@/components/StartupCard";
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -17,13 +20,28 @@ const md = markdownit();
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
 
-  const post = (await client.fetch(STARTUP_BY_ID_QUERY, {
-    id,
-  })) as StartUpTypeCard;
+  // Fetch the startup post and editor picks concurrently, using parallel requests.
+  const [post, { select: editorPicks }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, {
+      id,
+    }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
 
   if (!post) {
     return notFound();
   }
+
+  // Sequential requests can be slow, so we use Promise.all to fetch both queries concurrently.
+  // const post = (await client.fetch(STARTUP_BY_ID_QUERY, {
+  //   id,
+  // })) as StartUpTypeCard;
+
+  // const { select: editorPicks } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+  //   slug: "editor-picks",
+  // });
 
   const parsedContent = md.render(post?.pitch || "");
 
@@ -83,6 +101,18 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <hr className="divider" />
 
         {/* Editor recommended startups */}
+        {editorPicks?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPicks.map((startup: StartUpTypeCard, index: number) => (
+                <StartupCard key={startup._id + index} {...startup} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           {/* Fetch and display recommended startups here */}
           <View id={id} />
